e1082180c9e32a6af8d513aa84b06992866b8b6c
kenneth0castillo <kennethcastillo26@yahoo.com>
2018-08-05 14:31:12 -0400

    'update'

--- style/script/myscript.js
+++ style/script/myscript.js
@@ -48,14 +48,14 @@ function showSlides(n) {
 let slideIndexs = 0;
 showSlides();
 
-// function showSlides() {
-//     var i;
-//     var slides = document.getElementsByClassName("mySlides");
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none"; 
-//     }
-//     slideIndex++;
-//     if (slideIndex > slides.length) {slideIndex = 1} 
-//     slides[slideIndex-1].style.display = "block"; 
-//     setTimeout(showSlides, 2000); // Change image every 2 seconds
-// }
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
